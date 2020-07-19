29b869f869169222ece0b395858af9238a02de99
Joshua Elahie <lexelite36@gmail.com>
2020-07-18 23:56:07 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        lime: {
+          "100": "#e0ffc0",
+          "200": "#60c030",
+          "300": "#408020",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
